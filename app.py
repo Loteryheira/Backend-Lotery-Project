7362fa3b147835c762cd4b6cbb7a1693c0e95e86
Adrian Rincon <adrianrincon102001@gmail.com>
2025-03-06 16:46:18 -0400

    Estructura completa del proyecto

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ CORS(app)
 app.register_blueprint(chatbot_api)
 
 if __name__ == "__main__":
-    app.run(debug=True , port=5000, host="0.0.0.0")
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
