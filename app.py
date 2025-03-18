5a4a795b6ae730542b05ca7fec285a87e1bd4603
Adrian Rincon <adrianrincon102001@gmail.com>
2025-03-18 11:16:55 -0400

    ajuste de flujo y integracion completa de las capturas de pantalla

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ CORS(app)
 app.register_blueprint(chatbot_api)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
