e58120c5c06b9e615f4805dd3b3135c088fdb5d7
Adrian Rincon <adrianrincon102001@gmail.com>
2025-03-18 18:07:33 -0400

    ajuste de flujo y integracion completa de las capturas de pantalla

--- app.py
+++ app.py
@@ -18,8 +18,15 @@ app.register_blueprint(chatbot_api)
 # Configurar el nivel de logging
 app.logger.setLevel(logging.DEBUG)
 
-# Configurar el logger principal si necesitas ver más información del sistema
-logging.basicConfig(level=logging.DEBUG)
+# Configurar el logger principal con formato y fecha
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# Reducir el nivel de logs para PyMongo
+logging.getLogger("pymongo").setLevel(logging.WARNING)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
